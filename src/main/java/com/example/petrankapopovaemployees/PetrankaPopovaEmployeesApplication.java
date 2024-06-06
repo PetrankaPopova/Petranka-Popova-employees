d1f9680af0147a7d2ad93991f3aab3123d1874f4
Pepi <petranka_popova_@abv.bg>
2024-06-06 11:28:47 +0300

    Initial commit

---
+++ src/main/java/com/example/petrankapopovaemployees/PetrankaPopovaEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.petrankapopovaemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrankaPopovaEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrankaPopovaEmployeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petrankapopovaemployees/PetrankaPopovaEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petrankapopovaemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrankaPopovaEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
