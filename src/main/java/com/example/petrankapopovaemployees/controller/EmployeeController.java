8ec7df15c4ca3fd0b5bb8cef740dd2a8d0448577
Pepi <petranka_popova_@abv.bg>
2024-06-08 17:31:58 +0300

    Added additional functionality

--- src/main/java/com/example/petrankapopovaemployees/controller/EmployeeController.java
+++ src/main/java/com/example/petrankapopovaemployees/controller/EmployeeController.java
@@ -4,21 +4,13 @@ import com.example.petrankapopovaemployees.entity.EmployeePair;
 import com.example.petrankapopovaemployees.entity.EmployeeProject;
 import com.example.petrankapopovaemployees.entity.UploadResponse;
 import com.example.petrankapopovaemployees.service.EmployeeService;
-import com.opencsv.CSVReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
